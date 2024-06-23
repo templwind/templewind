f77c8086d535f112455229ac11c1ae011832933e
Alma Tuck <alma.tuck@gmail.com>
2024-06-23 16:24:42 -0700

    updating webserver

--- webserver/server.go
+++ webserver/server.go
@@ -52,6 +52,7 @@ func MustNewServer(c WebServerConf, opts ...ServerOpt) *Server {
 func (s *Server) Start() {
 	// Start server
 	go func() {
+		fmt.Printf("%s:%d\n\n", s.conf.Host, s.conf.Port)
 		if err := s.Echo.Start(fmt.Sprintf("%s:%d", s.conf.Host, s.conf.Port)); err != nil && err != http.ErrServerClosed {
 			log.Fatalf("Shutting down the server: %v", err)
 		}
