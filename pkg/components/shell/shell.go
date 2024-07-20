17b5081cfb2621a38d4e7a61e753fc36baa69c66
Alma Tuck <alma.tuck@gmail.com>
2024-07-19 18:00:43 -0600

    added shell

--- pkg/components/shell/shell.go
+++ pkg/components/shell/shell.go
@@ -7,6 +7,13 @@ import (
 
 // Props for the shell component
 type Props struct {
+	ID           string
+	Header       templ.Component
+	SidebarLeft  templ.Component
+	SidebarRight templ.Component
+	PageHeader   templ.Component
+	PageFooter   templ.Component
+	Footer       templ.Component
 }
 
 // New creates a new component
@@ -25,5 +32,49 @@ func WithProps(props ...templwind.OptFunc[Props]) *Props {
 }
 
 func defaultProps() *Props {
-	return &Props{}
+	return &Props{
+		ID: "shell",
+	}
+}
+
+func WithID(id string) templwind.OptFunc[Props] {
+	return func(p *Props) {
+		p.ID = id
+	}
+}
+
+func WithHeader(c templ.Component) templwind.OptFunc[Props] {
+	return func(p *Props) {
+		p.Header = c
+	}
+}
+
+func WithSidebarLeft(c templ.Component) templwind.OptFunc[Props] {
+	return func(p *Props) {
+		p.SidebarLeft = c
+	}
+}
+
+func WithSidebarRight(c templ.Component) templwind.OptFunc[Props] {
+	return func(p *Props) {
+		p.SidebarRight = c
+	}
+}
+
+func WithPageHeader(c templ.Component) templwind.OptFunc[Props] {
+	return func(p *Props) {
+		p.PageHeader = c
+	}
+}
+
+func WithPageFooter(c templ.Component) templwind.OptFunc[Props] {
+	return func(p *Props) {
+		p.PageFooter = c
+	}
+}
+
+func WithFooter(c templ.Component) templwind.OptFunc[Props] {
+	return func(p *Props) {
+		p.Footer = c
+	}
 }
